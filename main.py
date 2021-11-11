a3725e457a500febc8ade3271b768340f5a03ed6
quantummus <92730692+quantummus@users.noreply.github.com>
2021-11-11 16:37:37 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,31 +1,31 @@
 from vkbottle.bot import Bot, Message
-import asyncio
 from vkbottle import LoopWrapper
 
 bot = Bot("ba11cc5d4ee4bd92628131613bc774856efcb57c2b89b44295941c51ca249da41c22fd69fae0f61aabd32")
 bot.labeler.vbml_ignore_case = True
 
+grouplist = ["420-1", "420-2", "420-3", "420-4"]
+
 @bot.on.message(text="Привет")
-async def hi_handler(message: Message):
-    users_info = await bot.api.users.get(message.from_id)
-    await message.answer("Привет, {}".format(users_info[0].first_name))
+async def hi_handler(ans: Message):
+    users_info = await bot.api.users.get(ans.from_id)
+    await ans.answer("Привет, {}".format(users_info[0].first_name))
 
-@bot.on.message(text="Подписаться на рассылку")
-async def post_yes_handler(message: Message):
-    await message.answer("Вы успешно подписались на рассылку, но её пока что еще нет.")
-
-@bot.on.message(text="Отписаться от рассылки")
-async def post_no_handler(message: Message):
-    await message.answer("Вы успешно отписались нот рассылки, но какой был в этом смысл, если она и так пока еще не существует?")
-
-@bot.on.private_message(text="<msg>")
-async def echo_answer(ans: Message, msg):
-    await ans.answer("Ты написал: %s"%(msg))
+@bot.on.private_message(text="тест")
+async def post_yes_handler(ans: Message):
+    await ans.answer("В какой группе ты учишься?")
+    @bot.on.private_message(text="<group>")
+    async def echo_answer(ans: Message, group):
+        if group in grouplist:
+           await ans.answer("Ты учишься в %s группе" % (group))
+        else:
+            users_info = await bot.api.users.get(ans.from_id)
+            await ans.answer("Нет такой группы, {}!".format(users_info[0].first_name))
 
 lw = LoopWrapper()
 
 @lw.interval(seconds=10)
-async def repeated_task():
-    await Message.answer("гав гав")
+async def repeated_task(ans: Message):
+    await ans.answer("гав гав")
 
 bot.run_forever()

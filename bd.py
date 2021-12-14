693bfe052af8a0cb16237baa27f186b6934e2abe
NaraCrein <93971757+NaraCrein@users.noreply.github.com>
2021-12-14 14:36:03 +0700

    Add files via upload

--- bd.py
+++ bd.py
@@ -12,7 +12,7 @@ def sql_connection():
         print(Error)
 
 
-# функция для создания БД
+# функция для создания БД, если ее не существовало до этого
 def create_table():
     users = sql_connection()
     cursorUsr = users.cursor()
@@ -28,39 +28,98 @@ def create_table():
                         """)
     # тут вставка расписания с парсинга(?), пока вставила рандомные значения
     data = [(1, '420-1',
-             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.; 13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
              'today'),
             (2, '420-1',
-             '8:50-10:25: ТВиМС Практика рк 338 Лугина Н.Э.; 10:40-12:15 ОПР Практика ф 432а, ф 432б Петкун Т.А.16:45-18:20: ОПД Практика - -;',
+             '8:50-10:25: ТВиМС Практика рк 338 Лугина Н.Э.;\n\n10:40-12:15 ОПР Практика ф 432а, ф 432б Петкун Т.А.16:45-18:20: ОПД Практика - -;',
              'tomorrow'),
             (3, '420-1', 'thisweek_sch', 'thisweek'), (4, '420-1', 'nextweek_sch', 'nextweek'),
             (5, '420-2',
-             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.; 13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
              'today'),
             (6, '420-2', '16:45-18:20: ОПД Практика - -;', 'tomorrow'), (7, '420-2', 'thisweek_sch', 'thisweek'),
             (8, '420-2', 'nextweek_sch', 'nextweek'),
             (9, '420-3',
-             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.; 13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
              'today'),
             (10, '420-3', '16:45-18:20: ОПД Практика - -;', 'tomorrow'), (11, '420-3', 'thisweek_sch', 'thisweek'),
             (12, '420-3', 'nextweek_sch', 'nextweek'),
             (13, '420-4',
-             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.; 13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
              'today'),
             (14, '420-4', '16:45-18:20: ОПД Практика - -;', 'tomorrow'), (15, '420-4', 'thisweek_sch', 'thisweek'),
             (16, '420-4', 'nextweek_sch', 'nextweek')]
     cursorTim.executemany("INSERT OR IGNORE INTO timetable VALUES(?,?,?,?)", data)
     timetable.commit()
 
-
-def insert_user(group_name: str, subsc):
+# функция для вставки пользователя в бд и изменения группы
+def insert_user(i, group_name: str, subsc):
+    users = sql_connection()
+    cursorUsr = users.cursor()
     if subsc is None:
         subsc = 'no'
-    i = randint(0, 999999999)
-    user_info = (i, group_name, subsc)
-    cursorUsr.execute("INSERT INTO users VALUES(?,?,?)",
-                      user_info)  # вставка пользователя в таблицу(id, group)
+    user_info = (i[0].id, group_name, subsc)
+    cursorUsr.execute("""SELECT * FROM users WHERE user_id = ?""", (i[0].id,))
+    select = cursorUsr.fetchone()
+    if select == None:
+        cursorUsr.execute("INSERT INTO users VALUES(?,?,?)",
+                          user_info)  # вставка пользователя в таблицу(id, group)
+        users.commit()
+    else:
+        cursorUsr.execute("UPDATE users SET group_name = ? WHERE user_id =?", (group_name, i[0].id,))
+        users.commit()
+
+# функция для удаления пользователя
+def delete_user(user_id: int):
+    users = sql_connection()
+    cursorUsr = users.cursor()
+    cursorUsr.execute("""DELETE FROM users WHERE user_id = ?""", (user_id,))
     users.commit()
+
+
+def delete_all():
+    users = sql_connection()
+    cursorUsr = users.cursor()
+    cursorUsr.execute("""DELETE FROM users""")
+    users.commit()
+
+
+def get_schedule(group: str, date: str):
+    timetable = sql_connection()
+    cursorTim = timetable.cursor()
+    cursorTim.execute("""SELECT schedule FROM timetable WHERE group_name = ? AND date_id =?""", (group[0], date,))
+    schedule = cursorTim.fetchone()
+    return schedule[0]
+
+
+def update_schedule():  # функция для обновления расписания(вписывает значения data в бд)
+    timetable = sql_connection()
+    cursorTim = timetable.cursor()
+    cursorTim.execute("""DELETE FROM timetable""")
+    data = [(1, '420-1',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             'today'),
+            (2, '420-1',
+             '8:50-10:25: ТВиМС Практика рк 338 Лугина Н.Э.;\n\n10:40-12:15 ОПР Практика ф 432а, ф 432б Петкун Т.А.16:45-18:20: ОПД Практика - -;',
+             'tomorrow'),
+            (3, '420-1', 'thisweek_sch', 'thisweek'), (4, '420-1', 'nextweek_sch', 'nextweek'),
+            (5, '420-2',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             'today'),
+            (6, '420-2', '16:45-18:20: ОПД Практика - -;', 'tomorrow'), (7, '420-2', 'thisweek_sch', 'thisweek'),
+            (8, '420-2', 'nextweek_sch', 'nextweek'),
+            (9, '420-3',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             'today'),
+            (10, '420-3', '16:45-18:20: ОПД Практика - -;', 'tomorrow'), (11, '420-3', 'thisweek_sch', 'thisweek'),
+            (12, '420-3', 'nextweek_sch', 'nextweek'),
+            (13, '420-4',
+             '8:50-10:25: ТВиМС Лекция - Лугина Н.Э.;\n\n13:15-14:50: Иностранный язык Практика рк 125, рк 125а, рк 127,… Серебрякова О.А.;',
+             'today'),
+            (14, '420-4', '16:45-18:20: ОПД Практика - -;', 'tomorrow'), (15, '420-4', 'thisweek_sch', 'thisweek'),
+            (16, '420-4', 'nextweek_sch', 'nextweek')]
+    cursorTim.executemany("INSERT INTO timetable VALUES(?,?,?,?)", data)
+    timetable.commit()
 
 
 # создание 2 таблиц
@@ -70,39 +129,7 @@ cursorUsr = users.cursor()
 cursorTim = timetable.cursor()
 create_table()
 
-grouplist = cursorUsr.execute("""SELECT group_name
-                                FROM users """)
-print(grouplist.fetchall())
-# insert_user('420-1', 'yes')
-# insert_user('420-2', None)
-# insert_user('420-3', 'yes')
-# insert_user('420-4', None)
-
-# cursorUsr.execute("SELECT * FROM users")
-# print(cursorUsr.fetchall())
-#
-# cursorUsr.execute("""SELECT * FROM users WHERE subscribe = 'yes'""")
-# print(cursorUsr.fetchall())
-
-# day = 'tomorrow'
-# m_group = cursorUsr.execute("SELECT group_name "
-#                             "FROM users "
-#                             "WHERE subscribe = 'yes'")
-
-# m_users = cursorUsr.execute("SELECT user_id "
-#                             "FROM users "
-#                             "WHERE subscribe = 'yes'")
-# m_timetable = cursorTim.execute("""SELECT users.user_id, timetable.schedule
-#                                 FROM timetable, users
-#                                 WHERE users.subscribe='yes' AND users.group_name=timetable.group_name AND 'tomorrow'=date_id""")
-# for user in m_users:
-#     user_id = user
-#     print(cursorTim.fetchone())
-
-# print('Our BD:')
-# cursorObj.execute("SELECT * FROM users")
-# cursorObj.execute("""SELECT id, group_name FROM users WHERE group_name = "420-3" """)
-# print(cursorObj.fetchall())
+update_schedule()
 
 users.close()
 timetable.close()
--- tusur_danil_bot.py
+++ tusur_danil_bot.py
@@ -3,8 +3,8 @@ from random import randint
 from vkbottle.bot import Bot, Message
 from vkbottle import Keyboard, KeyboardButtonColor, Text, EMPTY_KEYBOARD, VKAPIError
 
-
 from bd import *
+
 # ------------это не надо трогать---------------
 
 bot = Bot("ba11cc5d4ee4bd92628131613bc774856efcb57c2b89b44295941c51ca249da41c22fd69fae0f61aabd32")
@@ -12,89 +12,108 @@ bot.labeler.vbml_ignore_case = True
 
 # ------------это не надо трогать---------------
 
-# список групп для проверки существует ли указанная пользователем группа
+# подключение к бд
 sqlite3.connect('bot.db')
 users = sql_connection()
 timetable = sql_connection()
 cursorUsr = users.cursor()
 cursorTim = timetable.cursor()
 
+# список групп для проверки существует ли указанная пользователем группа
+grouplist = ["420-1", "420-2", "420-3", "420-4"]
 
-grouplist = cursorUsr.execute("""SELECT group_name
-                                FROM users """)
-print(grouplist)
 
 # меню со статичными кнопками в поле ввода
 # сообщения
 
-@bot.on.private_message(payload={ "cmd": "menu" })
+@bot.on.private_message(payload={"cmd": "menu"})
 async def keyboard_handler(message: Message):
     keyboard = Keyboard()
-    keyboard.add(Text("На сегодня", { "cmd": "today" }))
-    keyboard.add(Text("На завтра", { "cmd": "tommorow" }))
+    keyboard.add(Text("На сегодня", {"cmd": "today"}))
+    keyboard.add(Text("На завтра", {"cmd": "tomorrow"}))
     keyboard.row()
-    keyboard.add(Text("На эту неделю", { "cmd": "thisweek" }))
-    keyboard.add(Text("На следующую неделю", { "cmd": "nextweek" }))
+    keyboard.add(Text("На эту неделю", {"cmd": "thisweek"}))
+    keyboard.add(Text("На следующую неделю", {"cmd": "nextweek"}))
     keyboard.row()
-    keyboard.add(Text("Настройки", { "cmd": "settings" }))
+    keyboard.add(Text("Настройки", {"cmd": "settings"}))
     await message.answer("Выберите, на ", keyboard=keyboard)
 
 
 # хэндлеры с отправкой пользователю расписания
 # на выбранный день/период
 
-@bot.on.private_message(payload={ "cmd": "today" })
+@bot.on.private_message(payload={"cmd": "today"})
 async def today_handler(message: Message):
-    # group_id = cursorTim.execute("SELECT group FROM users WHERE id = "id" ")
-    await message.answer("Расписание на сегодня:"+cursorTim.fetchall())
+    i = await bot.api.users.get(message.from_id)
+    cursorUsr.execute("SELECT group_name FROM users WHERE user_id = ?", (i[0].id, ))
+    group = cursorUsr.fetchone()
+    text = get_schedule(group, 'today')
+    await message.answer("Расписание на сегодня("+group[0]+"): \n\n"+text)
     #
 
 
-@bot.on.private_message(payload={ "cmd": "tommorow" })
+@bot.on.private_message(payload={"cmd": "tomorrow"})
 async def tommorow_handler(message: Message):
-    await message.answer("Расписание на завтра:")
+    i = await bot.api.users.get(message.from_id)
+    cursorUsr.execute("SELECT group_name FROM users WHERE user_id = ?", (i[0].id,))
+    group = cursorUsr.fetchone()
+    text = get_schedule(group, 'tomorrow')
+    await message.answer("Расписание на завтра("+group[0]+"):\n\n"+text)
     #
 
 
-@bot.on.private_message(payload={ "cmd": "thisweek" })
+@bot.on.private_message(payload={"cmd": "thisweek"})
 async def thisweek_handler(message: Message):
-    await message.answer("Расписание на эту неделю:")
+    i = await bot.api.users.get(message.from_id)
+    cursorUsr.execute("SELECT group_name FROM users WHERE user_id = ?", (i[0].id,))
+    group = cursorUsr.fetchone()
+    text = get_schedule(group, 'thisweek')
+    await message.answer("Расписание на эту неделю("+group[0]+"):\n\n"+text)
 
 
-@bot.on.private_message(payload={ "cmd": "nextweek" })
+@bot.on.private_message(payload={"cmd": "nextweek"})
 async def nextweek_handler(message: Message):
-    await message.answer("Расписание на следующую неделю:")
+    i = await bot.api.users.get(message.from_id)
+    cursorUsr.execute("SELECT group_name FROM users WHERE user_id = ?", (i[0].id,))
+    group = cursorUsr.fetchone()
+    text = get_schedule(group, 'nextweek')
+    await message.answer("Расписание на следующую неделю("+group[0]+"):\n\n"+text)
 
 
 # хэндлеры с подпиской пользователя на рассылку
 # и отпиской тоже
 
-@bot.on.private_message(payload={ "cmd": "sub" })
+@bot.on.private_message(payload={"cmd": "sub"})
 async def sub_handler(message: Message, group=None):
     # добавление долбоёба в базу
-    cursorUsr.execute("INSERT INTO users VALUES(?,?,?)", randint(0, 9999999999), group, 'yes')  # вставка пользователя в таблицу(id, group, subscribe)
+    i = await bot.api.users.get(message.from_id)
+    cursorUsr.execute("""UPDATE users SET subscribe = 'yes' WHERE user_id = ?""", (i[0].id, ))
+    users.commit()
     await message.answer("Вы успешно подписались на рассылку")
 
 
-@bot.on.private_message(payload={ "cmd": "unsub" })
+@bot.on.private_message(payload={"cmd": "unsub"})
 async def unsub_handler(message: Message):
     # удаление нищего дауна из базы данных тут
-
+    i = await bot.api.users.get(message.from_id)
+    cursorUsr.execute("""UPDATE users SET subscribe = 'no' WHERE user_id = ?""",
+                      (i[0].id,))
+    users.commit()
     await message.answer("Вы успешно отписались от рассылки")
 
 
 # меню с настройками
 
-@bot.on.private_message(payload={ "cmd": "settings" })
+@bot.on.private_message(payload={"cmd": "settings"})
 async def settings_handler(message: Message):
     keyboard = Keyboard()
-    keyboard.add(Text("Подписаться на рассылку", { "cmd": "sub" }))
+    keyboard.add(Text("Подписаться на рассылку", {"cmd": "sub"}))
     keyboard.row()
-    keyboard.add(Text("Отписаться от рассылки", { "cmd": "unsub" }))
+    keyboard.add(Text("Отписаться от рассылки", {"cmd": "unsub"}))
     keyboard.row()
-    keyboard.add(Text("Изменить номер группы", { "cmd": "askgroup" }))
+    keyboard.add(Text("Изменить номер группы", {"cmd": "askgroup"}))
     keyboard.row()
-    keyboard.add(Text("Вернуться в меню", { "cmd": "menu" }))
+    keyboard.add(Text("Вернуться в меню", {"cmd": "menu"}))
     await message.answer("Что вы хотите сделать?", keyboard=keyboard)
 
 
@@ -113,7 +132,7 @@ async def askgroup_handler(message: Message):
 
 # вылезает когда пользователь в настойках выбирает изменить свою группу
 
-@bot.on.private_message(payload={ "cmd": "askgroup" })
+@bot.on.private_message(payload={"cmd": "askgroup"})
 async def askgroup_handler(message: Message):
     await message.answer("Чтобы изменить номер группы, введите команду 'группа (номер_группы)'.")
 
@@ -126,11 +145,14 @@ async def askgroup_handler(message: Message):
 async def store_handler(message: Message, group=None):
     if group in grouplist:
         keyboard = Keyboard()
-        keyboard.add(Text("Продолжить", { "cmd": "menu" }))
+        keyboard.add(Text("Продолжить", {"cmd": "menu"}))
+        users_info = await bot.api.users.get(
+            message.from_id
+        )
 
-        cursorUsr.execute("INSERT INTO users VALUES(?,?,?)", randint(0, 9999999999), group, None)
+        insert_user(users_info, group, 'no')
         # вставка пользователя в таблицу(id, group, subscribe)
-        # получается каждый раз, когда он будет "менять группу", он будет менять свой id... Пока не вижу как это исправить
+        # получается каждый раз, когда он будет "менять группу", он будет менять свой id... Пока не вижу как это исправить(или все равно)
 
         await message.answer("Изменения сохранены!", keyboard=keyboard)
     else:
@@ -141,9 +163,7 @@ async def store_handler(message: Message, group=None):
 
 @bot.on.message(text="Привет")
 async def hi_handler(ans: Message):
-    users_info = await bot.api.users.get(
-        ans.from_id
-    )  # bot.api.users.get(ans.from_id) - бот собирает id пользователя. его нужно будет вставить в бд когда кто-то пишет "старт"
+    users_info = await bot.api.users.get(ans.from_id)  # bot.api.users.get(ans.from_id) - бот собирает id пользователя. его нужно будет вставить в бд когда кто-то пишет "старт"
     await ans.answer("Привет, {}".format(users_info[0].first_name))
 
 
